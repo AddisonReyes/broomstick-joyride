f88de2153052d3fd5ed37fd4798458b09ce41a46
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2025-11-21 12:59:51 -0400

    Menu loop created

--- src/main.js
+++ src/main.js
@@ -16,17 +16,52 @@ loadSprite("bean", "sprites/bean.png");
 
 loadFont("alagard", "fonts/alagard.ttf", { filter: "nearest" });
 
+// Utils
+function addButton(
+  label = "text",
+  position = vec2(200, 100),
+  f = () => debug.log("click")
+) {
+  const btn = add([
+    rect(240, 80, { radius: 6 }),
+    pos(position),
+    area(),
+    scale(1),
+    anchor("center"),
+    outline(4),
+    color(255, 255, 255),
+  ]);
+
+  btn.add([text(label), anchor("center"), color(0, 0, 0)]);
+
+  btn.onHoverUpdate(() => {
+    const t = time() * 10;
+    btn.color = hsl2rgb((t / 10) % 1, 0.6, 0.7);
+    btn.scale = vec2(1.1);
+    setCursor("pointer");
+  });
+
+  btn.onHoverEnd(() => {
+    btn.scale = vec2(1);
+    btn.color = rgb();
+  });
+
+  btn.onClick(f);
+
+  return btn;
+}
+
 // Game scenes
-scene("user", () => {
-  add([
+scene("user", ({ username }) => {
+  const titleLabel = add([
     pos(width() / 2, 64),
     text("Type your username", { align: "center", width: width() }),
     anchor("center"),
   ]);
 
-  const username = add([
-    text(""),
-    textInput(true, 12),
+  const usernameInput = add([
+    text(username),
+    textInput(true, 16),
     pos(width() / 2, height() / 2),
     anchor("center"),
   ]);
@@ -37,31 +72,48 @@ scene("user", () => {
     anchor("center"),
   ]);
 
-  username.onUpdate(() => {
-    if (username.text === "") {
+  usernameInput.onUpdate(() => {
+    if (usernameInput.text === "") {
       enterLabel.text = ". . .";
+      titleLabel.text = "Type your username";
     } else {
       enterLabel.text = "Press 'Enter' to continue...";
+      titleLabel.text = "";
     }
   });
 
   onKeyPress("enter", () => {
     if (enterLabel.text !== ". . .") {
-      go("menu", { username: username.text });
+      go("menu", { username: usernameInput.text });
     }
   });
 });
 
-scene("menu", () => {});
+scene("menu", ({ username }) => {
+  debug.log(username);
+  addButton("Play", vec2(width() / 2, height() / 2 - 64), () => {
+    go("game", { username: username });
+  });
+  addButton("User", vec2(width() / 2, height() / 2 + 32), () => {
+    go("user", { username: username });
+  });
+  addButton("Leaderboard", vec2(width() / 2, height() / 2 + 128), () => {
+    go("leaderboard", { username: username });
+  });
+});
 
-scene("game", () => {});
+scene("game", ({ username }) => {});
 
-scene("lose", () => {});
+scene("lose", ({ username }) => {});
 
-scene("leaderboard", () => {});
+scene("leaderboard", ({ username }) => {
+  addButton("Back", vec2(width() / 2, height() / 2 - 64), () => {
+    go("menu", { username: username });
+  });
+});
 
 function main() {
-  go("user", {});
+  go("menu", { username: "Dakotah" });
 }
 
 main();
