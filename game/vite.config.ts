db50d475050f88101b19ecebaea1452878aca046
AddisonReyes <addison.amin@gmail.com>
2026-04-27 12:00:49 -0400

    Testing new distribution itch.io method

--- game/vite.config.ts
+++ game/vite.config.ts
@@ -1,22 +1,5 @@
 import { defineConfig } from "vite";
 
-const kaplayCongrats = () => {
-  return {
-    name: "vite-plugin-kaplay-hello",
-    buildEnd() {
-      const line =
-        "---------------------------------------------------------";
-      const message =
-        "🦖 Awesome pal! Send your game to us:\n\n" +
-        "💎 Discord: https://discord.com/invite/aQ6RuQm3TF \n" +
-        "💖 Donate to KAPLAY: https://opencollective.com/kaplay\n" +
-        " (you can disable this msg on vite.config)";
-
-      process.stdout.write(`\n${line}\n${message}\n${line}\n`);
-    },
-  };
-};
-
 export default defineConfig({
   base: "./",
   server: {
@@ -32,5 +15,12 @@ export default defineConfig({
       },
     },
   },
-  plugins: [kaplayCongrats()],
+  plugins: [
+    {
+      name: "remove-crossorigin",
+      transformIndexHtml(html) {
+        return html.replace(/ crossorigin/g, "");
+      },
+    },
+  ],
 });
